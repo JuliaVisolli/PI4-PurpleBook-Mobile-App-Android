686a373d74d8ca8e4cf5f39d0dc03c401c628a8a
Adriana Diniz <dri.ragazoni@gmail.com>
2018-10-02 22:38:09 -0300

    Telas Login e Registro

--- app/src/main/java/com/example/adrianadrbezerra1/pi4/MainActivity.java
+++ app/src/main/java/com/example/adrianadrbezerra1/pi4/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.adrianadrbezerra1.pi4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Registre extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registre);
+        setContentView(R.layout.activity_main);
     }
 }
