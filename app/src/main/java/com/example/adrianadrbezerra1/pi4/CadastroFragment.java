60d8e0eed7ecdc7618c2329cd511b0efa44ca851
LittleWolf-PC <julia_viso@hotmail.com>
2018-10-06 18:26:55 -0300

    Validação dos campos do cadastro

--- app/src/main/java/com/example/adrianadrbezerra1/pi4/CadastroFragment.java
+++ app/src/main/java/com/example/adrianadrbezerra1/pi4/CadastroFragment.java
@@ -1,17 +1,31 @@
 package com.example.adrianadrbezerra1.pi4;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CadastroFragment extends Fragment {
+    private EditText etNome;
+    private EditText etSobrenome;
+    private EditText etEmail;
+    private EditText etSenha;
+    private Date dtNasc;
+    private RadioButton radioFem;
+    private RadioButton radioMasc;
+    private Button btnRegistro;
 
 
     public CadastroFragment() {
@@ -23,7 +37,51 @@ public class CadastroFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastro, container, false);
+        View v =  inflater.inflate(R.layout.fragment_cadastro, container, false);
+            etNome = v.findViewById(R.id.etNome);
+            etSobrenome = v.findViewById(R.id.etSobrenome);
+            etEmail = v.findViewById(R.id.etEmail);
+            etSenha = v.findViewById(R.id.etSenha);
+            radioFem = v.findViewById(R.id.radioFem);
+            radioMasc = v.findViewById(R.id.radioMasc);
+            btnRegistro = v.findViewById(R.id.btnRegistro);
+
+            View.OnClickListener listener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                if(etNome.getText().toString().isEmpty() || etSobrenome.getText().toString().isEmpty()
+                        || etEmail.getText().toString().isEmpty() || etSenha.getText().toString().isEmpty()){
+                    mensagem("Campo Obrigatório", "Atenção");
+
+                }
+            }
+        };
+        btnRegistro.setOnClickListener(listener);
+
+        return v;
+
+    }
+
+
+    private void mensagem(String message, String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder (CadastroFragment.this.getActivity());
+        //Configura o corpo da mensagem
+        builder.setMessage(message);
+        //Configura o título da mensagem
+        builder.setTitle(title);
+        //Impede que o botão seja cancelável (possa clicar
+        //em voltar ou fora para fechar)
+        builder.setCancelable(false);
+        //Configura um botão de OK para fechamento (um
+        //outro listener pode ser configurado no lugar do "null")
+        builder.setPositiveButton("OK", null);
+        //Cria efetivamente o diálogo
+        AlertDialog dialog = builder.create();
+        //Faz com que o diálogo apareça na tela
+        dialog.show();
+
     }
 
 }
--- app/src/main/java/com/example/adrianadrbezerra1/pi4/MainActivity.java
+++ app/src/main/java/com/example/adrianadrbezerra1/pi4/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.adrianadrbezerra1.pi4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.util.Date;
 
 public class MainActivity extends Activity {
+    private EditText etNome;
+    private EditText etSobrenome;
+    private EditText etEmail;
+    private Date dtNasc;
+    private RadioButton radioFem;
+    private RadioButton radioMasc;
+    private Button btnRegistro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        btnRegistro.setOnClickListener(listener);
     }
 }
