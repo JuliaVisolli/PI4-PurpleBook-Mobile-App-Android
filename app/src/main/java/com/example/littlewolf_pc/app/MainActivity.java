3821d8498245de51f41f6d50c7d4a6f0ab16389d
Adriana Diniz <dri.ragazoni@gmail.com>
2018-10-23 21:56:35 -0300

    Criação de tela de menu

--- app/src/main/java/com/example/littlewolf_pc/app/MainActivity.java
+++ app/src/main/java/com/example/littlewolf_pc/app/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.littlewolf_pc.app;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      // LoginFragment lf = new LoginFragment();
-       //getSupportFragmentManager().beginTransaction().replace(R.id.layout, lf).commit();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+       LoginFragment lf = new LoginFragment();
+       getSupportFragmentManager().beginTransaction().replace(R.id.moldura, lf).commit();
     }
 }
