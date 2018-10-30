20357a34f875ae86cc8af18d157b9773568de78f
FranciniSilva <Senac@123>
2018-10-30 19:53:39 -0300

    Inserção de menu na aplicação

--- app/src/main/java/com/example/littlewolf_pc/app/MainActivity.java
+++ app/src/main/java/com/example/littlewolf_pc/app/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +21,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       LoginFragment lf = new LoginFragment();
+       getSupportFragmentManager().beginTransaction().replace(R.id.moldura, lf).commit();
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-       LoginFragment lf = new LoginFragment();
-       getSupportFragmentManager().beginTransaction().replace(R.id.moldura, lf).commit();
+
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        if (menuItem.isChecked()) {
+                            menuItem.setChecked(false);
+                        } else {
+                            menuItem.setChecked(true);
+                        }
+                        drawerLayout.closeDrawers();
+                        if (menuItem.getItemId() == R.id.moldura) {
+                            LoginFragment fragment = new LoginFragment();
+                            getSupportFragmentManager().beginTransaction().replace(
+                                    R.id.moldura, fragment).commit();
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+
+        drawerLayout = findViewById(R.id.drawer);
+        //abrir fechar menu
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.openDrawer, R.string.closeDrawer) {
+        };
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
