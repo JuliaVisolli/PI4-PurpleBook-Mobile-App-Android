00e9a1f6c675c39a29e6dc7b69f97ec43332a716
FranciniSilva <Senac@123>
2018-11-23 22:18:19 -0200

    Ajustes de perfil usuario

--- app/src/main/java/com/example/littlewolf_pc/app/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/littlewolf_pc/app/fragment/ProfileFragment.java
@@ -48,8 +48,8 @@ public class ProfileFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        moldura = view.findViewById(R.id.layoutDadosUsuario);
+        final View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        moldura = view.findViewById(R.id.linear);
 
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -57,37 +57,47 @@ public class ProfileFragment extends android.support.v4.app.Fragment {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+
         Integer idUsuario = UsuarioSingleton.getInstance().getUsuario().getId();
+        if(idUsuario != null){
+            ApiUsuario apiUsuario = retrofit.create(ApiUsuario.class);
+            Call<List<UsuarioDTO>> usuarioDTOCall = apiUsuario.perfilUsuario(String.valueOf(idUsuario));
 
-        ApiUsuario apiUsuario = retrofit.create(ApiUsuario.class);
-        Call<List<UsuarioDTO>> usuarioDTOCall = apiUsuario.perfilUsuario(String.valueOf(idUsuario));
+            Callback<List<UsuarioDTO>> hiListCallback = new Callback<List<UsuarioDTO>>() {
+                @Override
+                public void onResponse(Call<List<UsuarioDTO>> call, Response<List<UsuarioDTO>> response) {
+                    List<UsuarioDTO> perfilUsuario = response.body();
 
-        Callback<List<UsuarioDTO>> hiListCallback = new Callback<List<UsuarioDTO>>() {
-            @Override
-            public void onResponse(Call<List<UsuarioDTO>> call, Response<List<UsuarioDTO>> response) {
-                System.out.print(response.body());
-                List<UsuarioDTO> perfilUsuario = response.body();
+                    if(perfilUsuario != null && response.code() == 200){
+                        for (UsuarioDTO usuarioDTO: perfilUsuario) {
 
-                if(perfilUsuario != null && response.code() == 200){
-                    for (UsuarioDTO usuarioDTO: perfilUsuario) {
+                            TextView nomeUsuario = view.findViewById(R.id.nome);
+                            nomeUsuario.setText(usuarioDTO.getNome());
 
-                        if(usuarioDTO.getFoto() != null){
-                            addItem(usuarioDTO.getFoto().toString(), usuarioDTO.getNome(), usuarioDTO.getEmail());
+
+                            if(usuarioDTO.getHistoria() != null){
+
+                                if(usuarioDTO.getFoto() != null){
+    //                            addItem(usuarioDTO.getFoto().toString(), usuarioDTO.getNome(), usuarioDTO.getEmail());
+                                }
+                                addItem("https://cdn4.iconfinder.com/data/icons/web-app-flat-circular-icons-set/64/Iconos_Redondos_Flat_Usuario_Icn-512.png", usuarioDTO.getNome(), usuarioDTO.getHistoria().getData(),  usuarioDTO.getHistoria().getTexto(),
+                                        "https://st3.depositphotos.com/12985790/18246/i/450/depositphotos_182461084-stock-photo-anonymous.jpg", usuarioDTO.getHistoria().getTotalCurtidas().toString(), usuarioDTO.getHistoria().getTotalComentarios().toString());
+                            }
+
                         }
-                        addItem("https://cdn4.iconfinder.com/data/icons/web-app-flat-circular-icons-set/64/Iconos_Redondos_Flat_Usuario_Icn-512.png", usuarioDTO.getNome(), usuarioDTO.getEmail());
 
                     }
 
                 }
+                @Override
+                public void onFailure(Call<List<UsuarioDTO>> call, Throwable t) {
+                    t.printStackTrace();
 
-            }
-            @Override
-            public void onFailure(Call<List<UsuarioDTO>> call, Throwable t) {
-                t.printStackTrace();
+                }
+            };
+            usuarioDTOCall.enqueue(hiListCallback);
+        }
 
-            }
-        };
-        usuarioDTOCall.enqueue(hiListCallback);
 
 
 
@@ -98,22 +108,34 @@ public class ProfileFragment extends android.support.v4.app.Fragment {
 
 
 
-    private void addItem(String url, String nome, String email){
+    private void addItem(String url, String textoDoTitulo, Date textoDaHora, String textoDaMensagem, String imageURL, String quantidadeCurtida, String quantidadeComentario){
         CardView cardView;
 
         cardView = (CardView) LayoutInflater.from(this.getActivity())
-                .inflate(R.layout.dados_perfil,
+                .inflate(R.layout.card,
                         moldura, false);
 
 
-        TextView nomeUsuario = cardView.findViewById(R.id.nome);
-        nomeUsuario.setText(nome);
+        TextView titulo = cardView.findViewById(R.id.titulo);
+        titulo.setText(textoDoTitulo);
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        String dateString = format.format(textoDaHora);
+        TextView hora = cardView.findViewById(R.id.hora);
+        hora.setText(dateString);
+        TextView mensagem = cardView.findViewById(R.id.mensagem);
+        mensagem.setText(textoDaMensagem);
+        TextView quantCurtida = cardView.findViewById(R.id.contcurtida);
+        quantCurtida.setText(quantidadeCurtida);
+        TextView quantComentario = cardView.findViewById(R.id.contcomentario);
+        quantComentario.setText(quantidadeComentario + " comentarios");
+//        btnComentario = cardView.findViewById(R.id.btn_comentario);
+//        btnCurtida = cardView.findViewById(R.id.btn_curtida);
 
 
         moldura.addView(cardView);
 
-//        carregarImagemPerfil(imageURL, cardView);
-//        carregarImagemHistoria(url, cardView);
+        carregarImagemPerfil(imageURL, cardView);
+        carregarImagemHistoria(url, cardView);
     }
 
     private void carregarImagemHistoria(String url, CardView cardView){
