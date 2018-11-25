0fc61edd9d1f53d72f614cf1a30260997358e3bd
Your Name <you@example.com>
2018-11-25 17:36:52 -0200

    Chamando comentarios do servico na tela de comentarios

--- app/src/main/java/com/example/littlewolf_pc/app/activity/ComentarioActivity.java
+++ app/src/main/java/com/example/littlewolf_pc/app/activity/ComentarioActivity.java
@@ -17,20 +17,69 @@ import java.util.Date;
 import java.util.List;
 
 import com.example.littlewolf_pc.app.R;
+import com.example.littlewolf_pc.app.model.HistoriaDTO;
+import com.example.littlewolf_pc.app.model.UsuarioDTO;
+import com.example.littlewolf_pc.app.resource.ApiComentario;
+import com.example.littlewolf_pc.app.resource.ApiHistoria;
+import com.example.littlewolf_pc.app.utils.UsuarioSingleton;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ComentarioActivity extends AppCompatActivity {
 
     private Button btnComent;
     List<ComentarioDTO> comentarioDTOList;
+    List<ComentarioDTO>lstComentarios = new ArrayList<ComentarioDTO>();
 
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://josiasveras.azurewebsites.net")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comentario);
         btnComent = findViewById(R.id.btnComent);
+        ApiComentario apiComentario =
+                retrofit.create(ApiComentario.class);
+        Call<List<ComentarioDTO>> comentarioDTOCall = apiComentario.getAllComentariosByIdHistoria("1");
 
+        Callback<List<ComentarioDTO>> comentarioCallBack = new Callback<List<ComentarioDTO>>() {
+            @Override
+            public void onResponse(Call<List<ComentarioDTO>> call, Response<List<ComentarioDTO>> response) {
+                comentarioDTOList = response.body();
 
+                if (comentarioDTOList != null && response.code() == 200) {
+
+                    if(comentarioDTOList.size() > 0){
+
+                        for (ComentarioDTO comentarioDTO : comentarioDTOList) {
+
+                            lstComentarios.add(new ComentarioDTO(comentarioDTO.getId(), comentarioDTO.getTexto(), comentarioDTO.getData()));
+                        }
+                        AdapterListerComentario AdapterListerComentario = new AdapterListerComentario(lstComentarios, ComentarioActivity.this);
+                        ListView lista = findViewById(R.id.lista_comentarios);
+                        lista.setAdapter(AdapterListerComentario);
+
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<ComentarioDTO>> call, Throwable t) {
+                t.printStackTrace();
+
+            }
+        };
+        comentarioDTOCall.enqueue(comentarioCallBack);
         enviarMensagem();
 
 
@@ -38,33 +87,19 @@ public class ComentarioActivity extends AppCompatActivity {
     }
 
     public void enviarMensagem(){
-
-        comentarioDTOList = new ArrayList<>();
-
-        comentarioDTOList.add(new ComentarioDTO(0, "Nome", null));
-
-
-
-
+        
         ListView listView;
         listView = (ListView) LayoutInflater.from(this)
                 .inflate(R.layout.model_comentario,
                         null, false);
 
-        View.OnClickListener listener = new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                comentarioDTOList.add(new ComentarioDTO(2, "flw", new Date()));
-
-            }
-        };
         btnComent.setOnClickListener(new View.OnClickListener() {
                 ListView lista = findViewById(R.id.lista_comentarios);
-                AdapterListerComentario AdapterListerComentario = new AdapterListerComentario(comentarioDTOList, ComentarioActivity.this);
+                AdapterListerComentario AdapterListerComentario = new AdapterListerComentario(lstComentarios, ComentarioActivity.this);
             @Override
             public void onClick(View v) {
-                comentarioDTOList.add(new ComentarioDTO(2, "flw", new Date()));
+                lstComentarios.add(new ComentarioDTO(2, "flw", new Date()));
 
                 lista.setAdapter(AdapterListerComentario);
             }
--- app/src/main/java/com/example/littlewolf_pc/app/utils/UsuarioSingleton.java
+++ app/src/main/java/com/example/littlewolf_pc/app/utils/UsuarioSingleton.java
@@ -1,10 +1,12 @@
 package com.example.littlewolf_pc.app.utils;
 
+import com.example.littlewolf_pc.app.model.HistoriaDTO;
 import com.example.littlewolf_pc.app.model.UsuarioDTO;
 
 public class UsuarioSingleton {
     private static final UsuarioSingleton INSTANCE = new UsuarioSingleton();
     private UsuarioDTO usuario;
+    private HistoriaDTO historia;
 
     private UsuarioSingleton(){}
 
@@ -18,5 +20,13 @@ public class UsuarioSingleton {
 
     public void setUsuario(UsuarioDTO usuario) {
         this.usuario = usuario;
+    }
+
+    public HistoriaDTO getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(HistoriaDTO historia) {
+        this.historia = historia;
     }
 }
